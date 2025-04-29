ed7f2e798ac9b4df5a1490031c75a0bd60e29177
Dion Weiman <dion@4axissolutions.com>
2025-04-29 17:13:54 +0530

    Added Category Delete Functionality

--- src/main/java/com/djw/devtiroblog/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/djw/devtiroblog/services/impl/CategoryServiceImpl.java
@@ -3,11 +3,13 @@ package com.djw.devtiroblog.services.impl;
 import com.djw.devtiroblog.domain.entities.Category;
 import com.djw.devtiroblog.repositories.CategoryRepository;
 import com.djw.devtiroblog.services.CategoryService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +30,21 @@ public class CategoryServiceImpl implements CategoryService {
         }
 
         return categoryRepository.save(category);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCategory(UUID id) {
+        Category category = getCategoryById(id);
+
+        if(!category.getPosts().isEmpty())
+            throw new IllegalStateException("Cannot Delete Category : " + category.getName() + " it has associated posts");
+
+        categoryRepository.delete(category);
+    }
+
+    private Category getCategoryById(UUID id){
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Category not found with id: " + id));
     }
 }
