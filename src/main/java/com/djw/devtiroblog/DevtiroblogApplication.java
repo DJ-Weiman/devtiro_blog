7751b6e7e0411bc8b4742de2269936a8d99a7871
Dion Weiman <dion@4axissolutions.com>
2025-04-29 09:46:40 +0530

    Initial Setup

---
+++ src/main/java/com/djw/devtiroblog/DevtiroblogApplication.java
@@ -0,0 +1,13 @@
+package com.djw.devtiroblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtiroblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtiroblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djw/devtiroblog/DevtiroblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djw.devtiroblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtiroblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
